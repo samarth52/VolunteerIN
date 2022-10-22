7ae9c594cf7bc4df013fd3d0bd1e390f53c604d8
Nitin Paul <67251286+paulni123@users.noreply.github.com>
2022-10-22 01:34:07 -0400

    finished base of dashboard

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -4,9 +4,14 @@ import styles from "../styles/ProfileCard.module.css";
 const ProfileCard = () => {
   return (
     <div class={styles.flexboxCandidate}>
-        <img src="components\images\ngoLogo.png" width="100" height="100"/>
+        <img src="https://i.ibb.co/1ncn6Hh/ngoLogo3.jpg" class={styles.imageStyle} width="100" height="100"/>
+        <h2 class={styles.profileText}>Samrat Sahoo</h2>
+        <h3 class={styles.profileText2}>Software Engineer</h3>
+        <h3 class={styles.profileText3}>10 Years of Experience</h3>
     </div>
   )
 }
 
 export default ProfileCard
+
+// "https://i.ibb.co/Y3QvCYc/ngoLogo2.jpg"
