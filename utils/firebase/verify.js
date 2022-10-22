c1fca94317014c0d04426d18fcf3286aeb3b4740
Samarth Chandna <samarthchandna52@gmail.com>
2022-10-22 01:57:39 -0400

    removed console page

--- utils/firebase/verify.js
+++ utils/firebase/verify.js
@@ -2,7 +2,6 @@ import auth from "./firebaseServer";
 
 const decodeIdToken = async (idToken) => {
   const decodedToken = await auth.verifyIdToken(idToken);
-  console.log(decodedToken);
   return decodedToken.email;
 };
 
