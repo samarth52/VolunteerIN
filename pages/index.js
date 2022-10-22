d989f662be56b83e94b8c8502c9c5370b04b85b1
Samarth Chandna <samarthchandna52@gmail.com>
2022-10-22 18:05:44 -0400

    removed eslint and import error

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import styles from '../styles/Home.module.css'
 import LandingPage from '../components/LandingPage'
-import DashboardWrapper from '../components/DashboardWrapper'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        {/* <DashboardWrapper/> */}
         <LandingPage/>
     </div>
   )
