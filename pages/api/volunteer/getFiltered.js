925250fcf923704ae3e01b9c0785c77045003a10
Samarth Chandna <samarthchandna52@gmail.com>
2022-10-22 12:34:17 -0400

    added location filter

--- pages/api/volunteer/getFiltered.js
+++ pages/api/volunteer/getFiltered.js
@@ -13,36 +13,45 @@ async function handler (req, res) {
         yearsMin: int | undefined,
         yearsMax: int | undefined,
         interests: [string] | undefined,
+        locations: [string] | undefined,
       },
       ...
     }
   `
 
-  const { ageMin, ageMax, yearsMin, yearsMax, interests } = req.data;
+  const { ageMin, ageMax, yearsMin, yearsMax, interests, locations } = req.data;
 
   filterQuery = {
     age: {},
     years: {},
-  };
+  }
 
   currDate = new Date();
   if (ageMin) {
     filterQuery.age["$gte"] = new Date(currDate.getFullYear() - ageMin, curr.getMonth(), curr.getDate());
-  };
+  }
   if (ageMax) {
     filterQuery.age["$lte"] = new Date(currDate.getFullYear() - ageMax, curr.getMonth(), curr.getDate());
-  };
+  }
 
   if (yearsMin) {
     filterQuery.years["$gte"] = yearsMin;
-  };
+  }
   if (yearsMax) {
     filterQuery.years["$lte"] = yearsMax;
-  };
+  }
 
   if (interest) {
-    filterQuery.interests["$in"] = interests;
-  };
+    filterQuery.interests = { "$in": interests }
+  }
+
+  if (locations) {
+    const regex = locations.join("|");
+    filterQuery.location = {
+      "$regex": regex,
+      "$options": "i",
+    }
+  }
 
   const volunteers = await getFilteredVolunteers(req.email, filterQuery);
   res.status(200).json({ payload: volunteers });
