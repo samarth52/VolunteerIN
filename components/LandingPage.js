d7740023d879a5c3ea7a0c9d3bd1bcccf3e0c288
Nitin Paul <67251286+paulni123@users.noreply.github.com>
2022-10-22 02:22:12 -0400

    start of landing page

---
+++ components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles/LandingPage.module.css'
+
+const LandingPage = () => {
+  return (
+    <div class=""></div>
+  )
+}
+
+export default LandingPage
