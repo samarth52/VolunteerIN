d28ef71967d283758f649733539bc9704c2a7aae
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-10-23 04:12:11 -0400

    fixed header links

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../styles/Header.module.css";
+import { useRouter } from 'next/router'
 
 const Header = ({logoTitle}) => {
+  const router = useRouter();
+  const [role, setRole] = useState("volunteer");
+  useEffect(() => {
+    if (router.asPath.indexOf("organization") !== -1){
+      setRole("organization")
+    }
+  })
+
   return (
     <div class={styles.header}>
         <div class={styles.logo}>{logoTitle}</div>
         <div class={styles.flexContainer}>
-            <div class={styles.headerText}>Dashboard</div>
-            <div class={styles.headerText}>Explore</div>
-            <div class={styles.headerText}>Login</div>
+            <div class={styles.headerText} onClick={() => {
+              router.push(`/${role}/dashboard`)
+            }}>Dashboard</div>
+            {role === "organization" && <div class={styles.headerText} onClick={() => {
+              router.push(`/${role}/explore`)
+            }}>Explore</div>}
+            <div class={styles.headerText} onClick={() => {
+              router.push(`/`)
+            }}>Logout</div>
         </div>
     </div>
   )
