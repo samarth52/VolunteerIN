e07e48b8cf6b946df2585e84ce405621776f3425
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-10-22 20:05:21 -0400

    finished modal detail

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "../styles/ProfileCard.module.css";
+import Popup from 'reactjs-popup';
+import ProfileDetailModal from './ProfileDetailModal'
 
 const ProfileCard = ({img, name, job, exp}) => {
   return (
-    <div class={styles.flexboxCandidate}>
-        <img src={img} class={styles.imageStyle} width="100" height="100"/>
-        <h2 class={styles.profileText}>{name}</h2>
-        <h3 class={styles.profileText2}>{job}</h3>
-        <h3 class={styles.profileText3}>{exp}</h3>
-    </div>
+    <Popup trigger={<div class={styles.flexboxCandidate}>
+    <img src={img} class={styles.imageStyle} width="100" height="100"/>
+    <h2 class={styles.profileText}>{name}</h2>
+    <h3 class={styles.profileText2}>{job}</h3>
+    <h3 class={styles.profileText3}>{exp}</h3>
+</div>} modal>
+    <ProfileDetailModal
+          firstName="Samrat"
+          lastName="Sahoo"
+          interests={["Software Engineering", "Product", "Fundraising"]}
+          yearsOfExperience="10"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          profileImage="https://media.graphassets.com/CqNntlMDRgKm9mYPIRNC"
+          experiences={[
+            {organization: "Healing4Heroes",
+          title: "Engineering Manager",
+        "years": 15},
+        {organization: "Healing4Heroes",
+          title: "Engineering Manager",
+        "years": 15},{organization: "Healing4Heroes",
+        title: "Engineering Manager",
+      "years": 15}
+          ]}
+        ></ProfileDetailModal>
+</Popup>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import styles from '../styles/Home.module.css'
-import LandingPage from '../components/LandingPage'
-
+import 'reactjs-popup/dist/index.css';
+import LandingPage from '../components/LandingPage';
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <LandingPage/>
-    </div>
+    <LandingPage/>
   )
 }
