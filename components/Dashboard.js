4a037122234efcf523eb1ce0042f238e6d033231
Samrat Sahoo <samratsahoo2013@gmail.com>
2022-10-23 05:08:09 -0400

    added empty dashboard screen

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,10 +2,7 @@ import React, { useState } from 'react'
 import styles from "../styles/NGODashboard.module.css";
 import ProfileCard from './ProfileCard';
 
-const Dashboard = () => {
-
-  const [profiles, setProfiles] = useState([])
-
+const Dashboard = (props) => {
   const imgURL = "https://i.ibb.co/1ncn6Hh/ngoLogo3.jpg"
   const propName = "Samrat Sahoo"
   const propJob = "Software Engineer"
@@ -17,11 +14,21 @@ const Dashboard = () => {
     {img: imgURL, name: propName, job:propJob, exp: propExp}
   ]
 
+  const [profiles, setProfiles] = useState([])
+
+
   return (
-    <div class={[styles.flexboxContainer]}>
-        {mockData.map((profile, index) => (
+    <div class={[profiles.length === 0 ? styles.notSelectedContainer : styles.flexboxContainer]}>
+        {profiles.length !== 0 && profiles.map((profile, index) => (
           <ProfileCard viewOnly={true} key={index} img={profile.img} name={profile.name} job={profile.job} exp={profile.exp}/>
         ))}
+
+        {profiles.length === 0 &&
+        <div className={styles.notSelectedImage}>
+          <img className={styles.notSelectedImage} src="https://i.imgur.com/m2Y3bZW.png"/>
+          <div>The Search is On!</div>
+          {props.isUserDashboard && <div className={styles.subTextMargin}>Organizations are still searching for candidates.</div>}
+        </div>} 
     </div>
   )
 }
